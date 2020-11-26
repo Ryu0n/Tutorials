5addb564b40c1f3828192b823f855d82a2e92800
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-11-26 17:17:46 +0900

    template
    index - use template shortcut
    detail - raise http404

--- Projects/mysite/polls/views.py
+++ Projects/mysite/polls/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
 
 from .models import Question
 
+
 # Create your views here.
 def index(request):
-    lastest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in lastest_question_list])
-    # return HttpResponse("Hello, world. You're at the polls index.")
-    return HttpResponse(output)
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+
+    # Inject logic
+    # output = ', '.join([q.question_text for q in lastest_question_list])
+    # # return HttpResponse("Hello, world. You're at the polls index.")
+    # return HttpResponse(output)
+
+    # Load template
+    # template = loader.get_template('polls/index.html')
+    # context = {
+    #     'latest_question_list': latest_question_list,
+    # }
+    # return HttpResponse(template.render(context, request))
+
+    # Shortcut
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
-    response = "You're looking at question {0}."
-    return HttpResponse(response.format(question_id))
+    # response = "You're looking at question {0}."
+    # return HttpResponse(response.format(question_id))
+
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404('Question does not exist')
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
