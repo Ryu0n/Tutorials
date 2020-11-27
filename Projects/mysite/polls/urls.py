b0f1ed5e23ae5c1fe94176c34e00a1e00904d3b2
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-11-27 15:32:19 +0900

    -
    -

--- Projects/mysite/polls/urls.py
+++ Projects/mysite/polls/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
+# 네임스페이스 설정 - html의 url 템플릿 태그에 다른 앱들과 구분하기 위해 사용된다.
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('specifics/<int:question_id>/', views.detail, name='detail'),
     path('<int:question_id>/results/', views.results, name='results'),
     path('<int:question_id>/vote/', views.vote, name='vote'),
 
