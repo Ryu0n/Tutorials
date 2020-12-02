8c1ef8ff3f1aa3fa0d706703b3a385b557218d94
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-12-02 15:12:18 +0900

    -
    -

--- Projects/stock/stock_inquiry/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockInquiryConfig(AppConfig):
-    name = 'stock_inquiry'
--- Projects/stock/stock_prediction/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StockPredictionConfig(AppConfig):
-    name = 'stock_prediction'
