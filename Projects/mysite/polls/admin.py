f37f820d253dd0f37cccd81120fcc127bf13401b
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-12-04 16:35:47 +0900

    admin page customize
    ch 07

--- Projects/mysite/polls/admin.py
+++ Projects/mysite/polls/admin.py
@@ -3,5 +3,21 @@ from .models import Question, Choice
 
 
 # Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        ('Question information', {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']})
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Choice)
