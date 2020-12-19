408f1fe0627625e248e55f34775714bad0e87a9f
Ryu0n <32003817+Ryu0n@users.noreply.github.com>
2020-12-19 14:37:11 +0900

    Set state

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -41,12 +41,19 @@ class App extends Component {
         </Subject> */}
         <header>
           {/* onclick 은 javascript, onClick은 JSX */}
-          <h1><a href="/" onClick={function(e){
-            // onclick 속성의 callback이 호출되면 이벤트의 기본적인 동작(href(/)로 리다이렉트)을 수행한다.
-            console.log(e);
-            // debugger; // 크롬 브라우저가 이 키워드를 만나면 멈추고 기다린다.
-            e.preventDefault() // 이벤트 객체의 기본적인 동작(리다이렉트)을 막는다. 즉, 페이지를 reload하지 않고 역동적인 동작을 수행할 수 있는 것이다.
-            }}>{this.state.subject.title}</a></h1>
+          <h1><a href="/" onClick={
+            function(e){
+              // onclick 속성의 callback이 호출되면 이벤트의 기본적인 동작(href(/)로 리다이렉트)을 수행한다.
+              console.log(e);
+              // debugger; // 크롬 브라우저가 이 키워드를 만나면 멈추고 기다린다.
+              e.preventDefault() // 이벤트 객체의 기본적인 동작(리다이렉트)을 막는다. 즉, 페이지를 reload하지 않고 역동적인 동작을 수행할 수 있는 것이다.
+              // this.state.mode = 'welcome'; // 바인딩을 하지 않으면 this가 자기 자신(Component)를 가르키지 않는다. 하지만 바인딩을 해도 리액트에서는 이 뜻을 못알아 먹기 때문에..
+              // setState() 메소드를 사용해야 state를 변경할 수 있다.
+              this.setState({
+                'mode': 'welcome'
+              });
+            }.bind(this) // onClick 콜백 메소드에서 this 키워드를 참조할 수 있도록 바인딩
+          }>{this.state.subject.title}</a></h1>
           {this.state.subject.sub}    
         </header>
         <TOC data={this.state.contents}></TOC>
