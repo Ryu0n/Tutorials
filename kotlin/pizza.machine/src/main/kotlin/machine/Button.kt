8b6691787557c0a98b032e00a110e9ba85027752
Ryu0n <hsung951027@gmail.com>
2025-07-09 22:44:55 +0900

    u: Fix counting typo

--- kotlin/pizza.machine/src/main/kotlin/machine/Button.kt
+++ kotlin/pizza.machine/src/main/kotlin/machine/Button.kt
@@ -2,21 +2,30 @@ package org.example.machine
 
 import org.example.machine.event.ActivationEvent
 import org.example.machine.listener.ButtonEventListener
-import org.example.menu.Menu
+import org.example.menu.pizza.Pizza
 import org.example.menu.ingredient.Ingredient
 
 class Button(
-    val menu: Menu,
+    val menu: Pizza,
     override var isActive: Boolean = false,
 ) : ButtonEventListener {
-    var onPurchaseRequested: ((menu: Menu) -> Unit)? = null
+    var onPurchaseRequested: ((menu: Pizza) -> Unit)? = null
 
     fun isMoneyEnough(insertedMoney: Int): Boolean {
         return insertedMoney >= menu.price
     }
 
     fun isIngredientsEnough(ingredients: List<Ingredient>): Boolean {
-        return ingredients.containsAll(menu.ingredients)
+        val remainedIngredients = ingredients.toMutableList()
+        for (ingredient in menu.ingredients) {
+            val found = remainedIngredients.indexOfFirst { it.name == ingredient.name }
+            if (found > -1) {
+                remainedIngredients.removeAt(found)
+            } else {
+                return false // Not enough ingredients
+            }
+        }
+        return true
     }
 
     override fun onActivated(e: ActivationEvent) {
