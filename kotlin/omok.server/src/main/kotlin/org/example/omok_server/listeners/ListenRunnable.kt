856ebd18a613989b415faf8e6d12dd917ffc38ad
ryu0n <hsung951027@gmail.com>
2025-07-13 17:16:42 +0900

    c: Add multi properties packet de/serialization for server

--- kotlin/omok.server/src/main/kotlin/org/example/omok_server/listeners/ListenRunnable.kt
+++ kotlin/omok.server/src/main/kotlin/org/example/omok_server/listeners/ListenRunnable.kt
@@ -2,7 +2,9 @@ package org.example.omok_server.listeners
 
 import org.example.omok_server.packet.Packet
 import org.example.omok_server.packet.deserialize
+import org.example.omok_server.packet.deserializeMultiPropertyPacket
 import org.example.omok_server.packet.serialize
+import org.example.omok_server.packet.serializeMultiPropertyPacket
 import java.net.Socket
 
 
@@ -23,12 +25,17 @@ class ListenRunnable(private val socket: Socket) : Runnable {
                 }
 
                 val receivedMessageBytes = buffer.copyOf(bytesRead)
-                val packet = deserialize(receivedMessageBytes)
-                println("Received message: ${packet.message}")
+//                val packet = deserialize(receivedMessageBytes)
+//                println("Received message: ${packet.message}")
+                val mpPacket = deserializeMultiPropertyPacket(receivedMessageBytes)
+                println("Received message: ${mpPacket.message}, number: ${mpPacket.number}, flag: ${mpPacket.flag}")
 
-                val convertedMessageBytes = serialize(packet)
-                outputStream.write(convertedMessageBytes)
-                println("Sent bytes: ${convertedMessageBytes.joinToString(", ")}")
+//                val convertedMessageBytes = serialize(packet)
+//                outputStream.write(convertedMessageBytes)
+//                println("Sent bytes: ${convertedMessageBytes.joinToString(", ")}")
+                val convertedMpBytes = serializeMultiPropertyPacket(mpPacket)
+                outputStream.write(convertedMpBytes)
+                println("Sent bytes: ${convertedMpBytes.joinToString(", ")}")
             }
         } catch (e: Exception) {
             e.printStackTrace()
