2c6fcb1c9cc5b94ea6fd43027aefd1e4ec9fb60c
ryu0n <hsung951027@gmail.com>
2025-08-01 20:33:51 +0900

    u: Edit runnable class to support EXIT type packet & Add function that move player from game room to waiting room

--- kotlin/omok.server/src/main/kotlin/org/example/omok_server/listeners/OmokListenRunnable.kt
+++ kotlin/omok.server/src/main/kotlin/org/example/omok_server/listeners/OmokListenRunnable.kt
@@ -1,6 +1,7 @@
 package org.example.omok_server.listeners
 
 import org.example.omok_server.packet.AttendancePacket
+import org.example.omok_server.packet.ExitPacket
 import org.example.omok_server.packet.Packet
 import org.example.omok_server.packet.SetColorPacket
 import org.example.omok_server.packet.data.SetColorPacketData
@@ -14,37 +15,71 @@ class OmokListenRunnable(
     val gameRooms: MutableList<Room>,
     val player: Player,
 ) : Runnable {
+    fun removePlayerFromApplication() {
+        player.close()
+        val gameRoom = gameRooms.find { it.players.contains(player) }
+        if (gameRoom != null) {
+            gameRoom.removePlayer(player)
+            if (gameRoom.players.isEmpty()) {
+                gameRooms.remove(gameRoom)
+            }
+        } else if (waitingRoom.players.contains(player)) {
+            waitingRoom.removePlayer(player)
+        }
+    }
+
+    fun removePlayerFromGameRoom() {
+        val gameRoom = gameRooms.find { it.players.contains(player) }
+        if (gameRoom != null) {
+            println("Removing player ${player.id} from game room")
+            gameRoom.removePlayer(player)
+            if (gameRoom.players.isEmpty()) {
+                gameRooms.remove(gameRoom)
+            }
+            waitingRoom.addPlayer(player)
+        }
+    }
+
+    fun addPlayerToGameRoom(packet: AttendancePacket) {
+        val roomId = packet.packetData.roomId
+        if (roomId.isNotEmpty()) {
+            if (waitingRoom.players.contains(player)) {
+                waitingRoom.removePlayer(player)
+            }
+            val gameRoom = gameRooms.find { it.players.size < 2 } ?: GameRoom()
+            gameRoom.addPlayer(player)
+            if (!gameRooms.contains(gameRoom)) {
+                gameRooms.add(gameRoom)
+            }
+            val color = gameRoom.players.size.toString()
+            player.send(
+                SetColorPacket(
+                    SetColorPacketData(
+                        listOf(color)
+                    )
+                )
+            )
+        }
+    }
+
     override fun run() {
         try {
             while (true) {
                 val packet = player.receive()
                 if (packet !is Packet) {
-                    continue
+                    println("Received invalid packet from player ${player.id}: $packet")
+                    removePlayerFromApplication()
+                    break
                 }
-
                 if (packet is AttendancePacket) {
-                    val roomId = packet.packetData.roomId
-                    if (roomId.isNotEmpty()) {
-                        if (waitingRoom.players.contains(player)) {
-                            waitingRoom.removePlayer(player)
-                        }
-                        val gameRoom = gameRooms.find { it.players.size < 2 } ?: GameRoom()
-                        gameRoom.addPlayer(player)
-                        if (!gameRooms.contains(gameRoom)) {
-                            gameRooms.add(gameRoom)
-                        }
-                        val color = gameRoom.players.size.toString()
-                        player.send(
-                            SetColorPacket(
-                                SetColorPacketData(
-                                    listOf(color)
-                                )
-                            )
-                        )
-                    }
+                    addPlayerToGameRoom(packet)
+                    continue
+                } else if (packet is ExitPacket) {
+                    removePlayerFromGameRoom()
                     continue
                 }
 
+                // Broadcast the packet to all players in the game rooms and waiting room (e.g. Chats, moves)
                 gameRooms.forEach { room ->
                     if (room.players.contains(player)) {
                         room.broadcast(packet)
@@ -57,8 +92,7 @@ class OmokListenRunnable(
         } catch (e: Exception) {
             println("Error reading message from player ${player.id}: ${e.message}")
         } finally {
-            player.socket.close()
-            waitingRoom.removePlayer(player)
+            removePlayerFromApplication()
         }
     }
 }
