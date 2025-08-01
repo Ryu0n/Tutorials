dd1dab2dcb27ed42a555a6173f3a7dbad38d199b
ryu0n <hsung951027@gmail.com>
2025-08-01 20:34:23 +0900

    u: Add EXIT type packet

---
+++ kotlin/omok.server/src/main/kotlin/org/example/omok_server/packet/ExitPacket.kt
@@ -0,0 +1,15 @@
+package org.example.omok_server.packet
+
+import org.example.omok_server.packet.data.ExitPacketData
+
+class ExitPacket(
+    override val packetData: ExitPacketData,
+) : Packet(packetData) {
+    override fun toString(): String {
+        return "${startDelimeter}${PacketType.EXIT.name}${delimeter}${packetData.roomId}${endDelimeter}"
+    }
+
+    override  fun serialize(): ByteArray {
+        return toString().toByteArray()
+    }
+}
